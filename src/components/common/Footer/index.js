9773562d422a7c4caef2de50673b95fcd9ebdda2
Divya <diyadarbe@gmail.com>
2022-03-29 14:17:12 +0530

    created separate css file for the each component

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "../../../styles/styles.css"
-import "../../../styles/mediaQueries.css"
+import "../../../styles/styles.css";
+import "./footer.css";
 
 const Footer = () => {
   return (
